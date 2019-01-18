8a196eb1d6375efadfa38a9debf1fc5647682672
Jasmin Parent <guildwarsguy12@gmail.com>
2019-01-18 11:40:05 -0500

    Create FriendByAge.py

---
+++ FriendByAge.py
@@ -0,0 +1,16 @@
+from mrjob.job import MRJob
+
+class FriendByAge(MRJob):
+    def mapper(self, key, line):
+        (userID, name, age, friendsCount) = line.split(',')
+        yield age, friendsCount
+        
+    def reducer(self, age, friendsCount):
+        i, friendsTotal, count = 0,0,0
+        for i in friendsCount:
+            count += 1
+            friendsTotal += int(i)
+        yield age, friendsTotal / count
+        
+if __name__ == '__main__':
+    FriendByAge.run()
